0bef098d2ad2b87e02f98224ea482cd849810dae
zwi3rzak <frontmatend@gmail.com>
2022-04-09 12:09:48 +0200

    CssExtractplugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: ['./src/main.js', './src/style.css'],
@@ -11,7 +12,7 @@ module.exports = {
     rules: [
       {
         test: /.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
     ],
   },
@@ -19,5 +20,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
+    new MiniCssExtractPlugin(),
   ],
 };
