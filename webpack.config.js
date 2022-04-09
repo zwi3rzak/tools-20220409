1221ba09d26b706abc4e9c9a7bbc667a22098bba
zwi3rzak <frontmatend@gmail.com>
2022-04-09 11:57:22 +0200

    css loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,18 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/main.js',
+  entry: ['./src/main.js', './src/style.css'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
